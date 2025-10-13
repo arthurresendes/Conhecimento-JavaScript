8e0e828e81ea3d84643d34abf9317f6a2e7c2587
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-13 17:15:57 -0300

    atualizacao

---
+++ Curso/Variaveis/variaveis1.js
@@ -0,0 +1,10 @@
+var texto = 'Curso de JavaScript'
+
+var numeroInteiro = -7
+
+var numeroFlutuante = 5.5
+
+var boleano = true // True - verdadeiro , False - Falso
+
+document.write(texto)
+console.log(numeroFlutuante)
