91eb69985d7a86daf0e13ff3eda1767b01542f6e
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-20 16:51:01 -0300

    atualizacao

---
+++ Curso/Desafios/abc.js
@@ -0,0 +1,19 @@
+var a = 10
+var b = 20
+var c = null
+document.write(a)
+document.write("\n")
+document.write(b)
+document.write("\n")
+document.write(c)
+
+c = a
+a = b
+b = c
+document.write("\n")
+document.write(a)
+document.write("\n")
+document.write(b)
+document.write("\n")
+c = null
+document.write(c)
