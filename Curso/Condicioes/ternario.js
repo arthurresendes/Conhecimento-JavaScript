46464877d4addefdd55551e62f728de9e5ef627a
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-27 17:33:06 -0300

    atualização

--- Curso/Condicioes/ternario.js
+++ Curso/Condicioes/ternario.js
@@ -1,1 +1,9 @@
 /* var result = <condicao> ? <verdadeiro> : <falso> */
+
+
+var nota = parseFloat(prompt("Digite sua nota: "))
+
+const MEDIA = 6.0
+
+var res = (nota >= MEDIA) ? 'Aprovado': 'Reprovado'
+document.write(res)
