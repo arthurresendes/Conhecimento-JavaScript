049a09f94370bf506af9e0a0fbe4cca4271dcdc8
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-27 17:29:05 -0300

    atualização

--- Curso/Condicioes/switch.js
+++ Curso/Condicioes/switch.js
@@ -1,22 +1,22 @@
-num1 = parseInt(prompt("Digite número inteiro: "))
-num2 = parseInt(prompt("Digite outro número inteiro: "))
-opcao = prompt("Qual operação aritmetica você quer fazer: + ,- , *, /")
+var num1 = parseInt(prompt("Digite número inteiro: "))
+var num2 = parseInt(prompt("Digite outro número inteiro: "))
+var opcao = prompt("Qual operação aritmetica você quer fazer: + ,- , *, /")
 
 switch (opcao) {
     case '+':
-        res = num1 + num2
+        var res = num1 + num2
         document.write("<h1> Soma: " + res + "</h1>")
         break;
     case '-':
-        res = num1 - num2
+        var res = num1 - num2
         document.write("<h1> Subtração: " + res + "</h1>")
         break;
     case '*':
-        res = num1 * num2
+        var res = num1 * num2
         document.write("<h1> Multiplicação: " + res + "</h1>")
         break;
     case '/':
-        res = num1 / num2
+        var res = num1 / num2
         document.write("<h1> Divisão: " + res + "</h1>")
         break;
 
---
+++ Curso/Condicioes/ternario.js
@@ -0,0 +1,1 @@
+/* var result = <condicao> ? <verdadeiro> : <falso> */
