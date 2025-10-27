d0f2562a1990e780109f1d78f62152004d3a9e6c
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-27 17:57:42 -0300

    atualização

---
+++ Curso/Função/funcao02.js
@@ -0,0 +1,49 @@
+function calcula_media(nota1, nota2, nota3) {
+    var soma = nota1 + nota2 + nota3
+    var media = soma / 3
+
+    return media
+}
+
+
+function resultado(media, materia) {
+    if (media >= 6.0) {
+        document.write("Você passou na materia de " + materia + " com a media de: ", + media)
+    } else {
+        document.write("Você não passou na materia de " + materia + " com a media de: ", + media)
+    }
+}
+
+alert("As notas devem estar em formato float, ou seja com .")
+
+var nota1;
+do {
+    nota1 = parseFloat(prompt("Digite sua primeira nota: "));
+    if (isNaN(nota1)) {
+        alert("Entrada inválida. Digite um número válido. Digite novamente!");
+    }
+} while (isNaN(nota1)); 
+
+
+var nota2;
+do {
+    nota2 = parseFloat(prompt("Digite sua segunda nota: "));
+    if (isNaN(nota2)) {
+        alert("Entrada inválida. Digite um número válido. Digite novamente!");
+    }
+} while (isNaN(nota2));
+
+
+var nota3;
+do {
+    nota3 = parseFloat(prompt("Digite sua terceira nota: "));
+    if (isNaN(nota3)) {
+        alert("Entrada inválida. Digite um número válido. Digite novamente!");
+    }
+} while (isNaN(nota3));
+
+var materia = prompt("Notas marcadas , qual materia dessas notas: ")
+
+calculando = calcula_media(nota1, nota2, nota3)
+
+document.write(`<h1>  ${resultado(calculando, materia)} </h1>`)
