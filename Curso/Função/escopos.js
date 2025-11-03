c92629b0a64b2f57fc96979489f6341031921027
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-03 17:16:36 -0300

    atualização

---
+++ Curso/Função/escopos.js
@@ -0,0 +1,17 @@
+var serie = 'Friends' // Variavel global
+
+if(serie == 'Friends'){
+    var serie2 = 'Senhor dos aneis'
+}
+
+console.log(serie2)
+
+
+function x(){
+    var serie3 = 'The Boys'
+    console.log(serie)
+    console.log(serie2)
+    console.log(serie3)
+}
+
+x()
