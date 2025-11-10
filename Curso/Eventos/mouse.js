ed21101dcd9d3133678d8d9a5e1f0760e73b9fc2
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-10 18:01:05 -0300

    Atualizar

---
+++ Curso/Eventos/mouse.js
@@ -0,0 +1,11 @@
+function acao(){
+    alert('Evento disparado')
+}
+
+function acao1(){
+    alert('Elemento clicado')
+}
+
+function acao2(){
+    alert('Elemento saindo da região do elemento')
+}
