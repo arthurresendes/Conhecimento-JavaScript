9096ae3596a6b03ed399d54c07590dcd9634383a
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-10 21:27:24 -0300

    atualização

---
+++ Curso/DOM/manipualando_classe.js
@@ -0,0 +1,7 @@
+function modificaClasse(){
+    if(document.getElementById('mensagem').className == 'estilo1'){
+        document.getElementById('mensagem').className = 'estilo2'
+    }else{
+        document.getElementById('mensagem').className = 'estilo1'
+    }
+}
