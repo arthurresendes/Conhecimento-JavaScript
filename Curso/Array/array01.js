b5fd26cb8ddf0c3a887d24061fd20e0d6cadded9
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-17 17:01:04 -0300

    atualização

---
+++ Curso/Array/array01.js
@@ -0,0 +1,14 @@
+var lista_frutas = Array()
+lista_frutas[0] = 'Maça'
+lista_frutas[1] = 'Banana'
+lista_frutas[2] = 'Pera'
+
+document.write('<h1>Nomes lista frutas: </h1>')
+for(let i = 0; i < lista_frutas.length; i++){
+    document.write(lista_frutas[i] + '<br>')
+}
+
+document.write('<hr>')
+document.write('<h1>Nomes lista frutas: </h1>')
+var lista_nomes = Array('Arthur', 'Jose', 'Maria')
+document.write(lista_nomes)
