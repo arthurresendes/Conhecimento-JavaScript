bec6444a76d1746f58549b3211850d50459d09ef
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-17 17:16:59 -0300

    atualização

--- Curso/Array/array02_multidimensional.js
+++ Curso/Array/array02_multidimensional.js
@@ -3,6 +3,9 @@ var lista_coisas = Array()
 lista_coisas['frutas'] = Array('Maça','Banana','Pera','Abacaxi')
 lista_coisas['pessoas'] = []
 
+lista_coisas['frutas'].push('Jaca')
+lista_coisas['frutas'].unshift('Pera')
+
 
 lista_coisas['pessoas']['a'] = 'Arthur'
 lista_coisas['pessoas']['b'] = 'Maria'
