ebe11d19c21ef556680fe002bf3468d2bb75ffc2
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-17 16:45:47 -0300

    atualização

--- Curso/Calculadora/calculadora.js
+++ Curso/Calculadora/calculadora.js
@@ -1,3 +1,8 @@
 function calcular(tipo,valor){
+    if (tipo == 'acao'){
 
+    }else if(tipo == "valor"){
+        var valor_campo = document.getElementById('resultado').value
+        document.getElementById('resultado').value = valor_campo += valor
+    }
 }
