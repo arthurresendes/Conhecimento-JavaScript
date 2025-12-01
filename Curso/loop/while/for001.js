5e9f510af287b2d0a7c28e8895b1b7f3b1b228e5
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-01 15:02:35 -0300

    atualizar

---
+++ Curso/loop/while/for001.js
@@ -0,0 +1,6 @@
+number = parseInt(prompt("Digite um numero para ver sua tabuada: "))
+for(let i = 1; i <11; i++){
+    document.write(number + " x " + i + " = " + number*i)
+
+    document.write("<br><br>")
+}
