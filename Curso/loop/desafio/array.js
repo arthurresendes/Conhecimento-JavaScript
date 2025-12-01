b9f5ce78347411dff3b7916b72d49c1047c0ce16
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-01 15:15:37 -0300

    atualizar

---
+++ Curso/loop/desafio/array.js
@@ -0,0 +1,13 @@
+var lista = Array()
+lista[0] = 'Arthur'
+lista[1] = 'Jose'
+lista[2] = 'Matheus'
+lista[3] = 'Maria'
+lista[4] = 'Gustavo'
+lista[5] = 'Gabriel'
+
+for(let i = 0; i < lista.length;i++){
+    document.write("Resultado posição " + i + ": " + lista[i])
+    
+    document.write("<br><br>")
+}
