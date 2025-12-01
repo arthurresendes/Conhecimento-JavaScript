cace3f866acdc21bf309ebac5bbda48a8a65e276
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-01 15:05:45 -0300

    atualizar

---
+++ Curso/loop/while/array001.js
@@ -0,0 +1,10 @@
+var x = Array()
+x[0] = "Oi"
+x[1] = "Ola"
+x[2] = "Hello"
+
+for (let i = 0; i < x.length; i++){
+    document.write("Resultado posição " + i + ": " + x[i])
+    
+    document.write("<br><br>")
+}
