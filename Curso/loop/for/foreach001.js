2249f020e99a0090e1afdb50edb597f48d471976
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-08 17:43:24 -0300

    atualização

--- Curso/loop/for/foreach001.js
+++ Curso/loop/for/foreach001.js
@@ -1,6 +1,9 @@
 var x = ['Arthur', 'Gabriel','Jose']
 
-x.forEach(function(valor,indice,array){
+x.forEach(function(valor,indice){
+    if (valor == 'Jose'){
+        valor = 'Maria'
+    }
     document.write('indice ' + indice)
     document.write(' nome: ' + valor)
     document.write("<br><br>")
