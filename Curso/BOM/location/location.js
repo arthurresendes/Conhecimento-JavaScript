d47c0bc2eb2c98aa6f266c54c5c722cea1bc2c59
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-13 13:50:13 -0300

    atualizar

---
+++ Curso/BOM/location/location.js
@@ -0,0 +1,12 @@
+
+function mudarpag(){
+    window.location.href="https://github.com/arthurresendes"
+}
+function mudarpag2(){
+    window.location.href="https://github.com/arthurresendes/Aprendizado-JavaScript"
+}
+
+function recarregar(){
+    window.location.reload()
+    
+}
