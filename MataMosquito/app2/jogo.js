097e43309b303cd5bec10916c4f3fdd7363888ea
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-20 15:33:09 -0300

    Atualizar

--- MataMosquito/app2/jogo.js
+++ MataMosquito/app2/jogo.js
@@ -5,7 +5,27 @@ function ajustaTamanho(){
     altura = window.innerHeight
     largura = window.innerWidth
 
-    console.log(altura,largura)
+    console.log(largura,altura)
 }
 
 ajustaTamanho()
+
+function posicaoRandomica(){
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+
+    console.log(posicaoX,posicaoY)
+
+    var mosca = document.createElement('img')
+    mosca.src = "imagens/mosca.png"
+    mosca.className = "mosquito1"
+    mosca.style.left = posicaoX + 'px'
+    mosca.style.top = posicaoY + 'px'
+    mosca.style.position = 'absolute'
+
+    document.body.appendChild(mosca)
+}
