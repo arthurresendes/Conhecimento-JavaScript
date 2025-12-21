1a2cf9fbdc9093f8c37f46af8db54f0b88948048
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-21 12:34:26 -0300

    Atualizar

--- MarioGame/jogo.js
+++ MarioGame/jogo.js
@@ -1,18 +1,18 @@
 var largura = 0
 var altura = 0
 
-function ajustaTela(){
+function ajustaTela() {
     altura = window.innerHeight
     largura = window.innerWidth
 
-    console.log(largura,altura)
+    console.log(largura, altura)
 }
 
 ajustaTela()
 
-function posicaoRandomica(){
+function posicaoRandomica() {
 
-    if(document.getElementById('mario')){
+    if (document.getElementById('mario')) {
         document.getElementById('mario').remove()
     }
 
@@ -20,11 +20,11 @@ function posicaoRandomica(){
     var posicaoY = Math.floor(Math.random() * altura) - 90
     posicaoX = posicaoX < 0 ? 0 : posicaoX
     posicaoY = posicaoY < 0 ? 0 : posicaoY
-    console.log(posicaoX,posicaoY)
+    console.log(posicaoX, posicaoY)
 
     var mario = document.createElement('img')
     mario.src = "Mario.png"
-    mario.className = tamanhoAleatorio() + ' ' +  ladoAleatorio()
+    mario.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
     mario.style.left = posicaoX + 'px'
     mario.style.top = posicaoY + 'px'
     mario.style.position = 'absolute'
@@ -36,10 +36,10 @@ function posicaoRandomica(){
     console.log(ladoAleatorio())
 }
 
-function tamanhoAleatorio(){
+function tamanhoAleatorio() {
     var classe = Math.floor(Math.random() * 3)
 
-    switch(classe){
+    switch (classe) {
         case 0:
             return 'mario1'
         case 1:
@@ -50,10 +50,10 @@ function tamanhoAleatorio(){
 
 }
 
-function ladoAleatorio(){
+function ladoAleatorio() {
     var lado = Math.floor(Math.random() * 2)
 
-    switch(lado){
+    switch (lado) {
         case 0:
             return 'ladoA'
         case 1:
@@ -61,16 +61,48 @@ function ladoAleatorio(){
     }
 }
 
-function resTotalElementos(){
+function resTotalElementos() {
+    var container = document.createElement('div')
+    container.id = 'container'
+    document.body.appendChild(container)
+
+
+    var pergunta = document.createElement('label')
+    pergunta.innerHTML = 'Quantos Marios apareceram: '
+    pergunta.style.fontFamily = 'Trebuchet MS'
+
+
     var resposta = document.createElement('input')
     resposta.type = 'text'
     resposta.id = 'totalMario'
-    resposta.placeholder = 'Quantos marios apareceram ?';
+    resposta.placeholder = 'Quantos Marios apareceram?'
 
     var validar = document.createElement('input')
     validar.type = 'submit'
     validar.value = 'Enviar'
 
-    document.body.appendChild(resposta)
-    document.body.appendChild(validar)
+    var feedback = document.createElement('p')
+    feedback.style.fontFamily = 'Trebuchet MS'
+    feedback.id = 'feedback'
+
+    validar.onclick = function () {
+        mostrarResposta()
+    }
+
+    container.appendChild(pergunta)
+    container.appendChild(resposta)
+    container.appendChild(validar)
+    container.appendChild(feedback)
+
+}
+
+function mostrarResposta() {
+    var valor = Number(document.getElementById('totalMario').value)
+
+    if (valor === totalMarios) {
+        document.getElementById('feedback').innerText = 'Acertou, o Mario não esta atras do Armario'
+    } else {
+        document.getElementById('feedback').innerText =
+            `Errou! O correto era ${totalMarios}. O Mario está atras do armario...`
+    }
 }
