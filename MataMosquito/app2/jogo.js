c40a9c44853600228d97ff40c48ebf3c044785ff
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-27 20:44:49 -0300

    Vidas

--- MataMosquito/app2/jogo.js
+++ MataMosquito/app2/jogo.js
@@ -1,5 +1,7 @@
 var altura = 0
 var largura = 0
+var vidas = 1
+
 
 function ajustaTamanho(){
     altura = window.innerHeight
@@ -14,6 +16,12 @@ function posicaoRandomica(){
 
     if(document.getElementById('mosca')){
         document.getElementById('mosca').remove()
+        if(vidas > 3){
+            alert("GameOver")
+        }else{
+                document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+            vidas++
+        }
     }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90
@@ -29,6 +37,9 @@ function posicaoRandomica(){
     mosca.style.top = posicaoY + 'px'
     mosca.style.position = 'absolute'
     mosca.id = 'mosca'
+    mosca.onclick = function(){
+        this.remove()
+    }
 
     document.body.appendChild(mosca)
 
