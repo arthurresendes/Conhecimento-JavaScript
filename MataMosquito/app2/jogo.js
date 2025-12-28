c030f898933ce09926cc1f4975e6878538b689ef
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-27 21:13:35 -0300

    cronometro

--- MataMosquito/app2/jogo.js
+++ MataMosquito/app2/jogo.js
@@ -1,6 +1,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 15
 
 
 function ajustaTamanho(){
@@ -11,6 +12,18 @@ function ajustaTamanho(){
 }
 
 ajustaTamanho()
+
+
+var cronometro = setInterval(function(){
+    tempo -=1
+    if (tempo < 0){
+        clearInterval(cronometro)
+        clearInterval(criaMosca)
+        window.location.href = "vitoria.html"
+    }else{
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+    },1000)
 
 function posicaoRandomica(){
 
