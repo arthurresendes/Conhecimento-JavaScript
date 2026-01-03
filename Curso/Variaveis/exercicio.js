e9ba8af1682b60e9a7909b3d16dc2fad6d94ba5d
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-03 14:36:46 -0300

    Let exercicio

---
+++ Curso/Variaveis/exercicio.js
@@ -0,0 +1,17 @@
+var serie = 'Friends'
+
+if(true){
+    var serie = 'The Boys'
+    console.log(serie)
+}
+
+console.log(serie)
+
+let serie_let = 'Friends'
+
+if(true){
+    let serie_let = 'The Boys'
+    console.log(serie_let)
+}
+
+console.log(serie_let)
