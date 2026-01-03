d11336788fd276ac37c825fdebdb689ddf923c1b
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-03 14:50:06 -0300

    Parametros default

---
+++ Curso/Função/default.js
@@ -0,0 +1,5 @@
+function contarUmaHistoria(personagem='Chapuezinho vermelho', atividade='andar',nome_dono = 'Lobo mal'){
+    console.log(`Nome personagem: ${personagem}, ele adorava ${atividade}, seu dono é ${nome_dono}`)
+}
+
+contarUmaHistoria(undefined,'Aprontar', 'sua mãe')
