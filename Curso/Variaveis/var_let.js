6c7a8678a302bf947a2ab1456515aacdd07ac82e
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-03 14:33:48 -0300

    Let

---
+++ Curso/Variaveis/var_let.js
@@ -0,0 +1,15 @@
+
+function x(){
+    if(true){
+        let serie = 'Invencivel'
+        console.log(serie)
+    }
+}
+
+    x()
+    if(true){
+        let serie = 'Dexter'
+        console.log(serie)
+    }
+
+// Let preserva o escopo e var não
