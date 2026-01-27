5968289b3bd258d6f739e7376958efd6353ec7c8
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-27 19:53:46 -0300

    Movendo/Delteando pastas

--- Curso_em_video/Aula 11/condicao.js
+++
@@ -1,4 +0,0 @@
-var vel = 60
-if(vel >= 60){
-    console.log(`Sua velocidade é de ${vel} Km/h , use cinto e diriga com muito cuidado`)
-}else console.log(`Sua velocidade é de ${vel}km/h , continue assim`)
