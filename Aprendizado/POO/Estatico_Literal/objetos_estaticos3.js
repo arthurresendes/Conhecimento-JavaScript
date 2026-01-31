e7a8fd4f8555a7fd53eb27498af7d1a4d0cb39ef
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-31 14:12:03 -0300

    Objetos literais/estaticos inserindo novos campos

---
+++ Aprendizado/POO/Estatico_Literal/objetos_estaticos3.js
@@ -0,0 +1,16 @@
+let pessoa ={
+    nome: 'Arthur',
+    idade: 19
+}
+
+console.log(pessoa)
+
+pessoa.sexo = 'Maculino'
+
+console.log(pessoa)
+
+pessoa.dizerOi= () => 
+    console.log(`Ola, tudo bem`)
+
+
+pessoa.dizerOi()
