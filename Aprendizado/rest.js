cfd8b2099e1cf81f52f00bb01cbf9d205058d9fe
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-02-07 14:52:45 -0300

    Atualizar o rest.js

--- Aprendizado/rest.js
+++ Aprendizado/rest.js
@@ -1,9 +1,13 @@
-function somarTudo(...numeros) {
-  return numeros.reduce((total, atual) => total + atual, 0);
-}
-console.log(somarTudo(1, 2, 3, 4)); // Resultado: 10
-
 const [primeiro, ...resto] = [10, 20, 30, 40];
 // primeiro = 10, resto = [20, 30, 40]
 
 
+function nums(...p){
+    let res = 0
+    for(let i = 0; i < p.length; i++){
+        res += i
+    }
+    return res
+}
+
+console.log(nums(1,2,3,4,4,5,6))
