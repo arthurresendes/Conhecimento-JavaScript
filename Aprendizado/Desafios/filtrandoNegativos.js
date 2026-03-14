ba19e03a6cf639510a7d92a9a7e2adef6fb269e2
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-03-14 12:12:25 -0300

    Filter in array

---
+++ Aprendizado/Desafios/filtrandoNegativos.js
@@ -0,0 +1,12 @@
+function negatives(arrayNumber){
+    let newList = []
+    for(i = 0 ; i < arrayNumber.length; i++){
+        if(arrayNumber[i] < 0){
+            newList.push(arrayNumber[i])
+        }
+    }
+
+    return newList
+}
+
+console.log(negatives([1,2,3-2,3,1,-3]))
